0ad9ea2c630766f6daecad2a9920576eefa89d95
Mahdi Bchetnia <mahdi@outlook.com>
2016-11-11 17:44:41 +0900

    Added Kanna framework for parsing HTML/XML

---
+++ stts/Services/DigitalOcean.swift
@@ -0,0 +1,13 @@
+//
+//  DigitalOcean.swift
+//  stts
+//
+//  Created by inket on 11/11/16.
+//  Copyright © 2016 inket. All rights reserved.
+//
+
+import Cocoa
+
+class DigitalOcean: Service {
+
+}
