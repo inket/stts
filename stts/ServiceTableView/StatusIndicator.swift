19449acefcc08259b1d4c41bab0723f6f6a8ace6
Mahdi Bchetnia <mahdi@outlook.com>
2016-12-24 10:16:53 +0900

    Readjusted the red color of status icons

--- stts/ServiceTableView/StatusIndicator.swift
+++ stts/ServiceTableView/StatusIndicator.swift
@@ -47,5 +47,5 @@ class StatusColor {
     static var green = NSColor(calibratedRed: 0.46, green: 0.78, blue: 0.56, alpha: 1)
     static var blue = NSColor(calibratedRed: 0.24, green: 0.54, blue: 1, alpha: 0.8)
     static var orange = NSColor.orange
-    static var red = NSColor(calibratedRed: 0.9, green: 0.78, blue: 0.56, alpha: 1)
+    static var red = NSColor(calibratedRed: 0.9, green: 0.4, blue: 0.23, alpha: 1)
 }
