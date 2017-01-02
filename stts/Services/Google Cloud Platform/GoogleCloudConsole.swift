d44f19345a64963175956c4418e1f83dac319958
Mahdi Bchetnia <mahdi@outlook.com>
2017-01-02 19:42:35 +0900

    Added services for Google Cloud Platform
    with GoogleCloudPlatformStatusStore as a cache to prevent multiple requests to the same endpoint

---
+++ stts/Services/Google Cloud Platform/GoogleCloudConsole.swift
@@ -0,0 +1,10 @@
+//
+//  GoogleCloudConsole.swift
+//  stts
+//
+
+import Foundation
+
+class GoogleCloudConsole: GoogleCloudPlatform {
+    override var name: String { return "Google Cloud Console" }
+}
---
+++ stts/Services/Google Cloud Platform/GoogleCloudDataproc.swift
@@ -0,0 +1,10 @@
+//
+//  GoogleCloudDataproc.swift
+//  stts
+//
+
+import Foundation
+
+class GoogleCloudDataproc: GoogleCloudPlatform {
+    override var name: String { return "Google Cloud Dataproc" }
+}
