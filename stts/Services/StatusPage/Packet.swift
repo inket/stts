3824266dcd6372d926ed601ab242a37193f088bf
Damian Sieczkowski <damian@alimity.com>
2017-01-07 11:24:32 -0500

    Removed copyright lines, changed to secure status page, and removed name.

--- stts/Services/StatusPage/Packet.swift
+++ stts/Services/StatusPage/Packet.swift
@@ -2,14 +2,11 @@
 //  Packet.swift
 //  stts
 //
-//  Created by D on 1/6/17.
-//  Copyright © 2017 inket. All rights reserved.
 //
 
 import Foundation
 
 class Packet: StatusPageService {
-    override var name: String { return "Packet" }
-    override var url: URL { return URL(string: "http://packet.statuspage.io")! }
+    override var url: URL { return URL(string: "https://packet.statuspage.io")! }
     override var statusPageID: String { return "39f8vhy6rw5d" }
 }
