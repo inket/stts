7ae36f44bf0bd386ed0c6b0ac098903da36a1506
David Panofsky <dpanofsky@singleplatform.com>
2017-01-09 13:52:48 -0500

    smartystreets uses http for statuspage

--- stts/Services/StatusPage/SmartyStreets.swift
+++ stts/Services/StatusPage/SmartyStreets.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 class SmartyStreets: StatusPageService {
-    override var url: URL { return URL(string: "https://status.smartystreets.com")! }
+    override var url: URL { return URL(string: "http://status.smartystreets.com")! }
     override var statusPageID: String { return "q1z5r94tnt56" }
 }
