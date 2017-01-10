1cfa54feddc48f0878656ab4fdc39f1b26c2d881
Mahdi Bchetnia <mahdi@outlook.com>
2017-01-10 11:49:34 +0900

    Prefer service domain's status page, not statuspageio subdomain

--- stts/Services/StatusPage/Packet.swift
+++ stts/Services/StatusPage/Packet.swift
@@ -7,6 +7,6 @@
 import Foundation
 
 class Packet: StatusPageService {
-    override var url: URL { return URL(string: "https://packet.statuspage.io")! }
+    override var url: URL { return URL(string: "http://status.packet.net")! }
     override var statusPageID: String { return "39f8vhy6rw5d" }
 }
