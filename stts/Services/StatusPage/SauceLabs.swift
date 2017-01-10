4980f6b81c316913fd9a50d5178cd3c040cdb6a6
Mahdi Bchetnia <mahdi@outlook.com>
2017-01-10 11:49:05 +0900

    SauceLabs -> Sauce Labs

--- stts/Services/StatusPage/SauceLabs.swift
+++ stts/Services/StatusPage/SauceLabs.swift
@@ -6,6 +6,7 @@
 import Foundation
 
 class SauceLabs: StatusPageService {
+    override var name: String { return "Sauce Labs" }
     override var url: URL { return URL(string: "https://status.saucelabs.com")! }
     override var statusPageID: String { return "kd2w7ghdk56w" }
 }
