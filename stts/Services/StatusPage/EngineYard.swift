1bf7c0a265ce867a315b53d704df375f99743a98
Mahdi Bchetnia <mahdi@outlook.com>
2017-01-10 11:49:34 +0900

    Prefer service domain's status page, not statuspageio subdomain

--- stts/Services/StatusPage/EngineYard.swift
+++ stts/Services/StatusPage/EngineYard.swift
@@ -7,6 +7,6 @@ import Foundation
 
 class EngineYard: StatusPageService {
     override var name: String { return "Engine Yard" }
-    override var url: URL { return URL(string: "https://engineyard.statuspage.io")! }
+    override var url: URL { return URL(string: "http://status.engineyard.com")! }
     override var statusPageID: String { return "76sphw1bc50q" }
 }
