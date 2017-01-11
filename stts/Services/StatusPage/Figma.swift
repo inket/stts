280b737d2a4cf81b636bebdbc607a1467d165297
Mahdi Bchetnia <mahdi@outlook.com>
2017-01-11 16:54:27 +0900

    Added service: Figma

---
+++ stts/Services/StatusPage/Figma.swift
@@ -0,0 +1,11 @@
+//
+//  Figma.swift
+//  stts
+//
+
+import Foundation
+
+class Figma: StatusPageService {
+    override var url: URL { return URL(string: "https://status.figma.com")! }
+    override var statusPageID: String { return "rxpksf93ynw6" }
+}
