9283b62a6dde3aacd4e920ba930bd9fbcd5eadee
Mahdi Bchetnia <mahdi@outlook.com>
2017-01-21 15:53:59 +0900

    Use http URL for Reddit's status page as the certificate is invalid

--- stts/Services/StatusPage/Reddit.swift
+++ stts/Services/StatusPage/Reddit.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 class Reddit: StatusPageService {
-    override var url: URL { return URL(string: "https://www.redditstatus.com/")! }
+    override var url: URL { return URL(string: "http://www.redditstatus.com/")! }
     override var statusPageID: String { return "2kbc0d48tv3j" }
 }
