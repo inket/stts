f45a53c60c2389f29abc079a49680078fc97bb8a
Mahdi Bchetnia <mahdi@outlook.com>
2017-02-16 12:53:19 +0900

    Added Quandl to xcodeproj, use http url because of certificate issues

--- stts/Services/StatusPage/Quandl.swift
+++ stts/Services/StatusPage/Quandl.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 class Quandl: StatusPageService {
-    override var url: URL { return URL(string: "https://status.quandl.com")! }
+    override var url: URL { return URL(string: "http://status.quandl.com")! }
     override var statusPageID: String { return "dyfxchz1hcb1" }
 }
