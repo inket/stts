6ccc4a79c207f1374ddbdc496bd78ce55d5d08b0
Mahdi Bchetnia <mahdi@outlook.com>
2017-02-21 14:17:45 +0900

    Use statuspage.io url for DNSimple as status.dnsimple.com is just a redirect

--- stts/Services/StatusPage/DNSimple.swift
+++ stts/Services/StatusPage/DNSimple.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 class DNSimple: StatusPageService {
-    override var url: URL { return URL(string: "https://status.dnsimple.com")! }
+    override var url: URL { return URL(string: "https://dnsimple.statuspage.io")! }
     override var statusPageID: String { return "tjym90yyv2zt" }
 }
