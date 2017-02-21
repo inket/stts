65c2f6e02619ac55c258a1f47e9eb31b917b2da3
Mahdi Bchetnia <mahdi@outlook.com>
2017-02-21 10:35:39 +0900

    Added Contentful.swift to project; use http URL as https has wrong certificate

--- stts/Services/StatusPage/Contentful.swift
+++ stts/Services/StatusPage/Contentful.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 class Contentful: StatusPageService {
-    override var url: URL { return URL(string: "https://status.contentful.com")! }
+    override var url: URL { return URL(string: "http://status.contentful.com")! }
     override var statusPageID: String { return "4bv17htq00cz" }
 }
