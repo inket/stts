f3c97cea4fc874b39028383a5c4ed6b60634a795
Mahdi Bchetnia <mahdi@outlook.com>
2017-04-05 09:48:10 +0900

    Added Codecov.swift to project

--- stts/Services/StatusPage/Codecov.swift
+++ stts/Services/StatusPage/Codecov.swift
@@ -5,7 +5,7 @@
 
 import Foundation
 
-class CodecovIO: StatusPageService {
+class Codecov: StatusPageService {
     override var url: URL { return URL(string: "http://status.codecov.io")! }
     override var statusPageID: String { return "wdzsn5dlywj9" }
 }
