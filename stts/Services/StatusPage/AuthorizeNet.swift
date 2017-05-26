7261fe41b9b56cb937620468fb46efae4819caf1
Mahdi Bchetnia <mahdi@outlook.com>
2017-05-26 09:41:37 +0900

    Added AuthorizeNet.swift to project

--- stts/Services/StatusPage/AuthorizeNet.swift
+++ stts/Services/StatusPage/AuthorizeNet.swift
@@ -6,6 +6,7 @@
 import Foundation
 
 class AuthorizeNet: StatusPageService {
-    override var url: URL { return URL(string: "http://status.authorize.net")! }
+    override var name: String { return "Authorize.Net" }
+    override var url: URL { return URL(string: "https://status.authorize.net")! }
     override var statusPageID: String { return "py5v2nkvrdpm" }
 }
