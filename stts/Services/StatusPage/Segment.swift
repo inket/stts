d00e009d37ddb72ee1e3e88bb0a390d0a779739e
Mahdi Bchetnia <mahdi@outlook.com>
2017-05-26 11:55:52 +0900

    Added service: Segment

---
+++ stts/Services/StatusPage/Segment.swift
@@ -0,0 +1,11 @@
+//
+//  Segment.swift
+//  stts
+//
+
+import Foundation
+
+class Segment: StatusPageService {
+    override var url: URL { return URL(string: "https://status.segment.com")! }
+    override var statusPageID: String { return "yzw83vcxbysq" }
+}
