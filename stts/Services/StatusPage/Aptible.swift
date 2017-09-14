a576cac81104c86c3d81ef94f51a1fcfa4d3b12e
Mahdi Bchetnia <mahdi@outlook.com>
2017-09-15 08:11:07 +0900

    Added Aptible.swift to project / Use http URL as HTTPS certificate is invalid

--- stts/Services/StatusPage/Aptible.swift
+++ stts/Services/StatusPage/Aptible.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 class Aptible: StatusPageService {
-    override var url: URL { return URL(string: "https://status.aptible.com/")! }
+    override var url: URL { return URL(string: "http://status.aptible.com/")! }
     override var statusPageID: String { return "fmwgqnbnbc4r" }
 }
