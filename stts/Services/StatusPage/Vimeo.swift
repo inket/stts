07efc7842292811b678e49d7e3aa7b10f3d214ab
Mahdi Bchetnia <mahdi@outlook.com>
2018-03-01 12:11:34 +0900

    Use after-redirection URL for Vimeo

--- stts/Services/StatusPage/Vimeo.swift
+++ stts/Services/StatusPage/Vimeo.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 class Vimeo: StatusPageService {
-    override var url: URL { return URL(string: "http://status.vimeo.com")! }
+    override var url: URL { return URL(string: "http://www.vimeostatus.com")! }
     override var statusPageID: String { return "sccqh0pnqrh8" }
 }
