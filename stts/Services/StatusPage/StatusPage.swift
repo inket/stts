d82bd3e4eb189bb4fe062a7b335b8024fd852685
Mahdi Bchetnia <mahdi@outlook.com>
2018-04-28 15:26:02 +0900

    Added service: Atlassian Statuspage

---
+++ stts/Services/StatusPage/StatusPage.swift
@@ -0,0 +1,12 @@
+//
+//  StatusPage.swift
+//  stts
+//
+
+import Foundation
+
+class StatusPage: StatusPageService {
+    let name = "Atlassian Statuspage"
+    let url = URL(string: "https://metastatuspage.com")!
+    let statusPageID = "y2j98763l56x"
+}
