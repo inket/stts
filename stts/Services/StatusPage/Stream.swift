bd3e25d570a2f6256d0585e867c3b9725c2c28d8
Mahdi Bchetnia <mahdi@outlook.com>
2018-05-07 00:14:22 +0900

    Added service: Stream
    closes #52

---
+++ stts/Services/StatusPage/Stream.swift
@@ -0,0 +1,11 @@
+//
+//  Stream.swift
+//  stts
+//
+
+import Foundation
+
+class Stream: StatusPageService {
+    let url = URL(string: "http://status.getstream.io")!
+    let statusPageID = "gsms7w17y9wc"
+}
