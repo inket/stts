5a3f2e8a3303134059f0254f0eb500c388219ff6
Mahdi Bchetnia <mahdi@outlook.com>
2018-05-27 12:31:42 +0900

    Fixed service: MailChimp

---
+++ stts/Services/StatusCake/MailChimp.swift
@@ -0,0 +1,11 @@
+//
+//  MailChimp.swift
+//  stts
+//
+
+import Foundation
+
+class MailChimp: StatusCakeService {
+    let url = URL(string: "https://status.mailchimp.com")!
+    let publicID = "58JncZazfp"
+}
