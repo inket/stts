33a0d47543edc51451b712bd1742c7102c6a9077
Mahdi Bchetnia <mahdi@outlook.com>
2018-07-06 08:35:37 +0900

    Added service: Buildkite

---
+++ stts/Services/StatusPage/Buildkite.swift
@@ -0,0 +1,11 @@
+//
+//  Buildkite.swift
+//  stts
+//
+
+import Foundation
+
+class Buildkite: StatusPageService {
+    let url = URL(string: "https://www.buildkitestatus.com")!
+    let statusPageID = "ltljpr68dygn"
+}
