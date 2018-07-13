acd853c541267cad168a31dced6ae22dcd15fff9
Mahdi Bchatnia <inket@users.noreply.github.com>
2018-07-13 14:02:41 +0900

    Use https URL for Fastly

--- stts/Services/StatusPage/Fastly.swift
+++ stts/Services/StatusPage/Fastly.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 class Fastly: StatusPageService {
-    let url = URL(string: "http://status.fastly.com")!
+    let url = URL(string: "https://status.fastly.com")!
     let statusPageID = "889dh1w1xtt0"
 }
