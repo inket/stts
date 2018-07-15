27d1f8d9c291d6a32d36f5348cdee08f676a7767
Mahdi Bchatnia <inket@users.noreply.github.com>
2018-07-15 16:27:13 +0900

    Use http URL for Mailgun since it doesn't support https

--- stts/Services/StatusPage/Mailgun.swift
+++ stts/Services/StatusPage/Mailgun.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 class Mailgun: StatusPageService {
-    let url = URL(string: "https://status.mailgun.com")!
+    let url = URL(string: "http://status.mailgun.com")!
     let statusPageID = "6jp439mdyy0k"
 }
