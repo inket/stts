fafd63624529582b3b94769e2141cca02706cf1a
Roopak Venkatakrishnan <roopak.v@gmail.com>
2018-07-20 08:33:12 -0700

    Add Spoke status page

---
+++ stts/Services/StatusPage/Spoke.swift
@@ -0,0 +1,11 @@
+//
+//  Spoke.swift
+//  stts
+//
+
+import Foundation
+
+class Spoke: StatusPageService {
+    let url = URL(string: "https://status.askspoke.com")!
+    let statusPageID = "4fw9h14fz72w"
+}
