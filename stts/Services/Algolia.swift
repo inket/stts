378d865a9726b9a2adf6a9a67590506c9e9c0e67
Mahdi Bchetnia <mahdi@outlook.com>
2018-07-25 11:11:40 +0900

    Added support for Algolia, closes #60

---
+++ stts/Services/Algolia.swift
@@ -0,0 +1,63 @@
+//
+//  Algolia.swift
+//  stts
+//
+
+import Kanna
+
+class Algolia: Service {
+    private enum AlgoliaStatus: String, Codable {
+        case operational
+        case majorOutage = "major_outage"
+        case degradedPerformance = "degraded_performance"
+        case partialOutage = "partial_outage"
+
+        var serviceStatus: ServiceStatus {
+            switch self {
+            case .operational:
+                return .good
+            case .majorOutage:
+                return .major
+            case .degradedPerformance,
+                 .partialOutage:
+                return .minor
+            }
+        }
+
+        var statusMessage: String {
+            switch self {
+            case .operational:
+                return "Operational"
+            case .majorOutage:
+                return "Major outage"
+            case .degradedPerformance:
+                return "Degraded performance"
+            case .partialOutage:
+                return "Partial outage"
+            }
+        }
+    }
+
+    let url = URL(string: "https://status.algolia.com/")!
+
+    override func updateStatus(callback: @escaping (BaseService) -> Void) {
+        let apiURL = URL(string: "https://status.algolia.com/2/status/service/all/period/current")!
+
+        URLSession.shared.dataTask(with: apiURL) { [weak self] data, _, error in
+            guard let selfie = self else { return }
+            defer { callback(selfie) }
+            guard let data = data else { return selfie._fail(error) }
+
+            let json = try? JSONSerialization.jsonObject(with: data, options: [])
+            guard
+                let dict = json as? [String: Any],
+                let statusDict = dict["global"] as? [String: String],
+                let statusString = statusDict["status"],
+                let status = AlgoliaStatus(rawValue: statusString)
+            else { return selfie._fail("Unexpected data") }
+
+            self?.status = status.serviceStatus
+            self?.message = status.statusMessage
+        }.resume()
+    }
+}
