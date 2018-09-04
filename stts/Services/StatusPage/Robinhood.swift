2449bc9b88fe68c2ea9403bd7b6c07da5b4d582a
Mahdi Bchatnia <inket@users.noreply.github.com>
2018-09-04 10:31:35 -0400

    Updated syntax

--- stts/Services/StatusPage/Robinhood.swift
+++ stts/Services/StatusPage/Robinhood.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 class Robinhood: StatusPageService {
-    override var url: URL { return URL(string: "http://status.robinhood.com")! }
-    override var statusPageID: String { return "49plxygx5s1k" }
+    let url = URL(string: "https://status.robinhood.com")!
+    let statusPageID = "49plxygx5s1k"
 }
