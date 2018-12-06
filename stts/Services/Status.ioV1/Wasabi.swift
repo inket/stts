fe961b16be47052b8f1b1d1ce7239c64c90cb1dd
Mahdi Bchetnia <mahdi@outlook.com>
2018-12-06 22:29:22 +0900

    Added service: Wasabi, closes #65

---
+++ stts/Services/Status.ioV1/Wasabi.swift
@@ -0,0 +1,11 @@
+//
+//  Wasabi.swift
+//  stts
+//
+
+import Foundation
+
+class Wasabi: StatusioV1Service {
+    let url = URL(string: "https://status.wasabi.com")!
+    let statusPageID = "5abbc12aeb57a904e44a58d3"
+}
