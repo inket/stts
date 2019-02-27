3f6ed6b28efef507e7814ead2897db47fd197bb9
Mahdi Bchetnia <mahdi@outlook.com>
2019-02-27 11:46:52 +0900

    Added service: CloudApp, closes #71

---
+++ stts/Services/StatusPage/CloudApp.swift
@@ -0,0 +1,11 @@
+//
+//  CloudApp.swift
+//  stts
+//
+
+import Foundation
+
+class CloudApp: StatusPageService {
+    let url = URL(string: "http://status.getcloudapp.com")!
+    let statusPageID = "jv7lgzmdfxr1"
+}
