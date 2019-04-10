94ef09c866e5f3f97d139d8255d9940c61e78e8a
Mahdi Bchetnia <mahdi@outlook.com>
2019-04-10 10:36:00 +0900

    Added service: Urban Airship, closes #76

---
+++ stts/Services/StatusPage/UrbanAirship.swift
@@ -0,0 +1,12 @@
+//
+//  UrbanAirship.swift
+//  stts
+//
+
+import Foundation
+
+class UrbanAirship: StatusPageService {
+    let name = "Urban Airship"
+    let url = URL(string: "https://status.urbanairship.com")!
+    let statusPageID = "gk8jfj1gbrzn"
+}
