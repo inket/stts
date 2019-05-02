11ec4ca32739be658ae40cca5ba3b8a981ed8d3a
Mahdi Bchetnia <mahdi@outlook.com>
2019-05-02 17:03:52 +0100

    Use StatusPageService for Avalara and Recurly

---
+++ stts/Services/StatusPage/Avalara.swift
@@ -0,0 +1,11 @@
+//
+//  Avalara.swift
+//  stts
+//
+
+import Foundation
+
+class Avalara: StatusPageService {
+    let url = URL(string: "https://status.avalara.com")!
+    let statusPageID = "m10fpsk86bhy"
+}
---
+++ stts/Services/StatusPage/Recurly.swift
@@ -0,0 +1,11 @@
+//
+//  Recurly.swift
+//  stts
+//
+
+import Foundation
+
+class Recurly: StatusPageService {
+    let url = URL(string: "https://status.recurly.com")!
+    let statusPageID = "rq8zvx7w9n0c"
+}
