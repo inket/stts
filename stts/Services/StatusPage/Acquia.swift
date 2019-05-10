ab58a568b8186bd9ee27e7ae653c3b15f9b241b0
Mahdi Bchetnia <mahdi@outlook.com>
2019-05-10 19:22:21 +0900

    Added service: Acquia, closes #79

---
+++ stts/Services/StatusPage/Acquia.swift
@@ -0,0 +1,11 @@
+//
+//  Acquia.swift
+//  stts
+//
+
+import Foundation
+
+class Acquia: StatusPageService {
+    let url = URL(string: "https://status.acquia.com")!
+    let statusPageID = "c3bdbqpxhzcc"
+}
