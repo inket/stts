fe488f433b300a175f46ea7b97bb7ab749bda6d3
Alexandre Boucey <alexandre.boucey@gmail.com>
2019-05-14 16:27:27 +0200

    Add Asana service

---
+++ stts/Services/StatusPage/Asana.swift
@@ -0,0 +1,11 @@
+//
+//  Asana.swift
+//  stts
+//
+
+import Foundation
+
+class Asana: StatusPageService {
+    let url = URL(string: "https://trust.asana.com")!
+    let statusPageID = "w58858w0sjxx"
+}
