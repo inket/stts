2dfe947563ab8542b5784a28a86d2b83acb7a741
Mahdi Bchetnia <mahdi@outlook.com>
2019-06-12 07:36:01 +0900

    Use https URL for bitrise

--- stts/Services/StatusPage/Bitrise.swift
+++ stts/Services/StatusPage/Bitrise.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 class Bitrise: StatusPageService {
-    let url = URL(string: "http://status.bitrise.io")!
+    let url = URL(string: "https://status.bitrise.io")!
     let statusPageID = "1q8r4n02vb2k"
 }
