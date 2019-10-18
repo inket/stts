520c42e35cfa8deabd913f21d19fa505ce99c7d3
Gregory Daynes <gregdaynes@gmail.com>
2019-10-18 09:24:19 -0700

    Add LaunchDarkly Status Page

---
+++ stts/Services/StatusPage/LaunchDarkly.swift
@@ -0,0 +1,11 @@
+//
+//  LaunchDarkly.swift
+//  stts
+//
+
+import Foundation
+
+class LaunchDarkly: StatusPageService {
+    let url = URL(string: "https://status.launchdarkly.com")!
+    let statusPageID = "9404kdk7lkn7"
+}
