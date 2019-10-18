4b316eae031de51deb7daa491a0928359b9e8b07
Gregory Daynes <gregdaynes@gmail.com>
2019-10-18 10:43:43 -0700

    Add Mural StatusPage

---
+++ stts/Services/StatusPage/Mural.swift
@@ -0,0 +1,11 @@
+//
+//  Mural.swift
+//  stts
+//
+
+import Foundation
+
+class Mural: StatusPageService {
+    let url = URL(string: "https://status.mural.co")!
+    let statusPageID = "dbk70dpy3n7h"
+}
