a3a1e6337a75064aabbd4c89befa95231e613df4
Gregory Daynes <gregdaynes@gmail.com>
2019-10-18 10:38:21 -0700

    Add VictorOps Status Page

---
+++ stts/Services/StatusPage/VictorOps.swift
@@ -0,0 +1,11 @@
+//
+//  VictorOps.swift
+//  stts
+//
+
+import Foundation
+
+class VictorOps: StatusPageService {
+    let url = URL(string: "http://status.victorops.com")!
+    let statusPageID = "tj7j024mr40d"
+}
