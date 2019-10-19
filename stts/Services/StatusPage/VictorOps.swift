9fe1cebd7907ff1a62259690a308dfeb53deaac4
Mahdi Bchatnia <inket@users.noreply.github.com>
2019-10-19 13:01:49 +0900

    Use HTTPS url for VictorOps

--- stts/Services/StatusPage/VictorOps.swift
+++ stts/Services/StatusPage/VictorOps.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 class VictorOps: StatusPageService {
-    let url = URL(string: "http://status.victorops.com")!
+    let url = URL(string: "https://status.victorops.com")!
     let statusPageID = "tj7j024mr40d"
 }
