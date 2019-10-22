9a0f2e14d96eef22630bb11d155fe7ed787e1bb3
Mahdi Bchetnia <mahdi@outlook.com>
2019-10-22 23:14:37 +0900

    Added Python.swift to the project

--- stts/Services/StatusPage/Python.swift
+++ stts/Services/StatusPage/Python.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 class Python: StatusPageService {
-    override var url: URL { return URL(string: "https://status.python.org/")! }
-    override var statusPageID: String { return "2p66nmmycsj3" }
+    let url = URL(string: "https://status.python.org")!
+    let statusPageID = "2p66nmmycsj3"
 }
