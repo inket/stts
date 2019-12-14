dbe76ea11de2af501aebfa71060aacc5edf3ecbb
Mahdi Bchetnia <mahdi@outlook.com>
2019-12-14 19:10:24 +0900

    Added service Pantheon, closes #93

---
+++ stts/Services/StatusPage/Pantheon.swift
@@ -0,0 +1,11 @@
+//
+//  Pantheon.swift
+//  stts
+//
+
+import Foundation
+
+class Pantheon: StatusPageService {
+    let url = URL(string: "https://status.pantheon.io")!
+    let statusPageID = "80f0zgsx80zh"
+}
