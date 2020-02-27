aad08663874653fb1d0aa18feb20732927ae3ea3
Mahdi Bchetnia <mahdi@outlook.com>
2020-02-27 11:42:43 -0800

    Added service: Zoom, closes #98

---
+++ stts/Services/StatusPage/Zoom.swift
@@ -0,0 +1,11 @@
+//
+//  Zoom.swift
+//  stts
+//
+
+import Foundation
+
+class Zoom: StatusPageService {
+    let url = URL(string: "https://status.zoom.us")!
+    let statusPageID = "14qjgk812kgk"
+}
