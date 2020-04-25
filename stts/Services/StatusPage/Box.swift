21963ae675b3e2d75eb0b0cc3ebaafd7a43ee8f8
Brandon Bianchi <bbianchi@egnyte.com>
2020-04-24 17:01:49 -0700

    Added status checker for Box

---
+++ stts/Services/StatusPage/Box.swift
@@ -0,0 +1,11 @@
+//
+//  Box.swift
+//  stts
+//
+
+import Foundation
+
+class Egnyte: StatusPageService {
+    let url = URL(string: "https://status.box.com")!
+    let statusPageID = "208q92hckwws"
+}
