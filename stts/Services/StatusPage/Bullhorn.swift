1416bd5f1423950b399866438b9b5e11e0cf2d50
Mahdi Bchatnia <git@mahdi.jp>
2020-07-11 23:10:27 +0900

    Added service: Bullhorn, closes #109

---
+++ stts/Services/StatusPage/Bullhorn.swift
@@ -0,0 +1,11 @@
+//
+//  Bullhorn.swift
+//  stts
+//
+
+import Foundation
+
+class Bullhorn: StatusPageService {
+    let url = URL(string: "https://status.bullhorn.com")!
+    let statusPageID = "cdxw86qkmw3x"
+}
