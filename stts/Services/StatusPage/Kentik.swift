09ba3ea2d86f3ce71ebc397f294c1cb674ddc657
Brandon Bianchi <bbianchi@egnyte.com>
2020-08-07 13:42:21 -0700

    Added Kentik Status Page

---
+++ stts/Services/StatusPage/Kentik.swift
@@ -0,0 +1,11 @@
+//
+//  Kentik.swift
+//  stts
+//
+
+import Foundation
+
+class Kentik: StatusPageService {
+    let url = URL(string: "https://status.kentik.com")!
+    let statusPageID = "25fy3sprxcv3"
+}
