f7c7311bccd9ee4f4f2cab9d1c19ad99e4c0816a
Mahdi Bchatnia <git@mahdi.jp>
2020-10-18 16:39:48 +0900

    Fixed link for Pendo, closes #113

--- stts/Services/StatusPage/Pendo.swift
+++ stts/Services/StatusPage/Pendo.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 class Pendo: StatusPageService {
-    let url = URL(string: "https://status.pendo.io/api")!
+    let url = URL(string: "https://status.pendo.io")!
     let statusPageID = "qxvy69hcwh22"
 }
