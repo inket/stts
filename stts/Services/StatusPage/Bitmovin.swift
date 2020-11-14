003cd99939316508790190c74c373bd2b771516e
Mahdi Bchatnia <git@mahdi.jp>
2020-11-14 16:22:12 +0900

    Fixed Bitmovin

--- stts/Services/StatusPage/Bitmovin.swift
+++ stts/Services/StatusPage/Bitmovin.swift
@@ -1,11 +1,11 @@
-
 //
 //  Bitmovin.swift
 //  stts
 //
+
 import Foundation
 
-class Bolt: StatusPageService {
+class Bitmovin: StatusPageService {
     let url = URL(string: "https://status.bitmovin.com")!
     let statusPageID = "05q9dh7vx2mf"
 }
--- stts/Services/StatusPage/Recruiterbox.swift
+++ stts/Services/StatusPage/Recruiterbox.swift
@@ -6,7 +6,6 @@
 
 import Foundation
 
-
 class Recruiterbox: StatusPageService {
     let url = URL(string: "https://status.recruiterbox.com")!
     let statusPageID = "mnlrk0049tgf"
