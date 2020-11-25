cffc985600218b6150ee1ad201a844989b5c29b0
Alvin Yates <ayates@aurorasolar.com>
2020-11-25 12:19:40 -0800

    Add Statuspage for CoderPad

---
+++ stts/Services/StatusPage/CoderPad.swift
@@ -0,0 +1,11 @@
+//
+//  CoderPad.swift
+//  stts
+//
+
+import Foundation
+
+class CoderPad: StatusPageService {
+    let url = URL(string: "https://status.coderpad.io")!
+    let statusPageID = "29rb067ttd9j"
+}
