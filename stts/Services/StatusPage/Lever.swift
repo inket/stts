192095d69593a34202260d83c6ea789d31b99593
Alvin Yates <ayates@aurorasolar.com>
2020-12-15 14:46:23 -0800

    Add Lever.co

---
+++ stts/Services/StatusPage/Lever.swift
@@ -0,0 +1,11 @@
+//
+//  Lever.swift
+//  stts
+//
+
+import Foundation
+
+class Lever: StatusPageService {
+    let url = URL(string: "https://status.lever.co")!
+    let statusPageID = "34ygfdwr11jd"
+}
