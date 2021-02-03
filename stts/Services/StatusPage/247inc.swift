f454f185374336e34ec9b4ad343038f9116b919e
Brandon Bianchi <brandon@brandonbianchi.com>
2021-02-03 10:29:44 -0800

    Added 247inc statuspage

---
+++ stts/Services/StatusPage/247inc.swift
@@ -0,0 +1,11 @@
+//
+//  247inc.swift
+//  stts
+//
+
+import Foundation
+
+class Packet: StatusPageService {
+    let url = URL(string: "https://247inc.statuspage.io/")!
+    let statusPageID = "hpw5z45r09mj"
+}
