8b6c936f1ae6d49f27c97eefb4450acbbe3b63ef
Mahdi Bchatnia <git@mahdi.jp>
2021-02-12 00:46:31 +0900

    Added .swift files to project

--- stts/Services/StatusPage/FreeAgent.swift
+++ stts/Services/StatusPage/FreeAgent.swift
@@ -1,10 +1,11 @@
- //
- //  FreeAgent.swift
- //  stts
- //
- import Foundation
+//
+//  FreeAgent.swift
+//  stts
+//
 
- class FreeAgent: StatusPageService {
-     let url = URL(string: "https://status.freeagent.com/")!
-     let statusPageID = "tn2dcgpy021r"
- }
+import Foundation
+
+class FreeAgent: StatusPageService {
+    let url = URL(string: "https://status.freeagent.com/")!
+    let statusPageID = "tn2dcgpy021r"
+}
--- stts/Services/StatusPage/Semaphore.swift
+++ stts/Services/StatusPage/Semaphore.swift
@@ -1,10 +1,11 @@
 //
-//  SemaphoreCI.swift
+//  Semaphore.swift
 //  stts
 //
+
 import Foundation
 
-class SemaphoreCI: StatusPageService {
+class Semaphore: StatusPageService {
     let url = URL(string: "https://status.semaphoreci.com/")!
     let statusPageID = "zq6qk8cct5d8"
 }
