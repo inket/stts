2f12f8f526231b1969b24a4feaa104da8d1a51ff
Ro Bin-Wan Känobi <32737410+tau-ri@users.noreply.github.com>
2021-03-25 11:52:02 +0100

    add logzio service
    this is the status oveview of service logz.io

---
+++ stts/Services/StatusPage/logzio.swift
@@ -0,0 +1,11 @@
+//
+//  Logzio.swift
+//  stts
+//
+
+import Foundation
+
+class Logzio: StatusPageService {
+    let url = URL(string: "https://status.logz.io/")!
+    let statusPageID = "flbqdmg201t4"
+}
