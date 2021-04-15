4b4dcc025035e1e43a10a07922708435babb01f4
Alvin Yates <ayates@aurorasolar.com>
2021-04-15 13:26:23 -0700

    Add Statuspage for Mode Analytics

---
+++ stts/Services/StatusPage/ModeAnalytics.swift
@@ -0,0 +1,11 @@
+//
+//  ModeAnalytics.swift
+//  stts
+//
+
+import Foundation
+
+class ModeAnalytics: StatusPageService {
+    let url = URL(string: "https://status.modeanalytics.com/")!
+    let statusPageID = "th35d4vb90h8"
+}
