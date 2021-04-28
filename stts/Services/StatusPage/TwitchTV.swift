0f15c33f40e627a57c091c3af5c2805f74e3b857
Fabrizio Natale <Faber92@users.noreply.github.com>
2021-04-28 11:37:22 +0000

    feat: add twitch status page

---
+++ stts/Services/StatusPage/TwitchTV.swift
@@ -0,0 +1,11 @@
+//
+//  TwitchTV.swift
+//  stts
+//
+
+import Foundation
+
+class TwitchTV: StatusPageService {
+    let url = URL(string: "https://status.twitch.tv")!
+    let statusPageID = "yfj40zdsk34s"
+}
