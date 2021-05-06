c7123564a2484a70fa13698a2b125f1cfa8b6ca0
Mahdi Bchatnia <git@mahdi.jp>
2021-05-06 13:21:42 +0900

    Added service: Coinbase

---
+++ stts/Services/StatusPage/Coinbase.swift
@@ -0,0 +1,11 @@
+//
+//  Coinbase.swift
+//  stts
+//
+
+import Foundation
+
+class Coinbase: StatusPageService {
+    let url = URL(string: "https://status.coinbase.com")!
+    let statusPageID = "kr0djjh0jyy9"
+}
