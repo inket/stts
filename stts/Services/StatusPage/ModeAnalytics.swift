c06bb16f1c312b8a7989e8dc842835579e58f437
Mahdi Bchatnia <git@mahdi.jp>
2021-05-06 13:21:32 +0900

    Added ModeAnalytics/Twitch to project, fixed up names

--- stts/Services/StatusPage/ModeAnalytics.swift
+++ stts/Services/StatusPage/ModeAnalytics.swift
@@ -6,6 +6,7 @@
 import Foundation
 
 class ModeAnalytics: StatusPageService {
+    let name = "Mode Analytics"
     let url = URL(string: "https://status.modeanalytics.com/")!
     let statusPageID = "th35d4vb90h8"
 }
--- stts/Services/StatusPage/Twitch.swift
+++ stts/Services/StatusPage/Twitch.swift
@@ -1,11 +1,11 @@
 //
-//  TwitchTV.swift
+//  Twitch.swift
 //  stts
 //
 
 import Foundation
 
-class TwitchTV: StatusPageService {
+class Twitch: StatusPageService {
     let url = URL(string: "https://status.twitch.tv")!
     let statusPageID = "yfj40zdsk34s"
 }
