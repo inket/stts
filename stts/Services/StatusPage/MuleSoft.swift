a1c9a4a0b28ffd8f4209ff8b2fa9b35681efd717
Mahdi Bchatnia <git@mahdi.jp>
2021-07-11 16:50:30 +0900

    Added service: MuleSoft

---
+++ stts/Services/StatusPage/MuleSoft.swift
@@ -0,0 +1,11 @@
+//
+//  MuleSoft.swift
+//  stts
+//
+
+import Foundation
+
+class MuleSoft: StatusPageService {
+    let url = URL(string: "https://status.mulesoft.com")!
+    let statusPageID = "rl5vblzz3gbw"
+}
