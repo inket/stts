abe53923e1468d3070a65293b2e20402518c9ed5
Mahdi Bchatnia <git@mahdi.jp>
2021-07-11 16:49:05 +0900

    Added service: Pardot

---
+++ stts/Services/StatusPage/Pardot.swift
@@ -0,0 +1,11 @@
+//
+//  Pardot.swift
+//  stts
+//
+
+import Foundation
+
+class Pardot: StatusPageService {
+    let url = URL(string: "https://trust.pardot.com")!
+    let statusPageID = "j0hnsfnqtllz"
+}
