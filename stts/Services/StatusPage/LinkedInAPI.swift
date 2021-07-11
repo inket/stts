32dabe480c66b4e779b800f88527261d34e8e75f
Mahdi Bchatnia <git@mahdi.jp>
2021-07-12 08:30:19 +0900

    Added service: LinkedIn API

---
+++ stts/Services/StatusPage/LinkedInAPI.swift
@@ -0,0 +1,12 @@
+//
+//  LinkedInAPI.swift
+//  stts
+//
+
+import Foundation
+
+class LinkedInAPI: StatusPageService {
+    let name = "LinkedIn API"
+    let url = URL(string: "https://linkedin.statuspage.io")!
+    let statusPageID = "mxfydrt8b8xw"
+}
