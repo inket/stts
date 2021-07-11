cb672f588127a04633009533ecad5351ee79df7e
Mahdi Bchatnia <git@mahdi.jp>
2021-07-11 20:22:16 +0900

    Added services: Linear, Plausible, Railway, Sketch, Threader
    closes #139

---
+++ stts/Services/Instatus/Plausible.swift
@@ -0,0 +1,10 @@
+//
+//  Plausible.swift
+//  stts
+//
+
+import Foundation
+
+class Plausible: InstatusService {
+    let url = URL(string: "https://plausible.instatus.com")!
+}
---
+++ stts/Services/Instatus/Threader.swift
@@ -0,0 +1,10 @@
+//
+//  Threader.swift
+//  stts
+//
+
+import Foundation
+
+class Threader: InstatusService {
+    let url = URL(string: "https://threader.instatus.com")!
+}
