7f5db5a5d19dddbab9179552bb00e8be76ac1dfc
Mahdi Bchatnia <git@mahdi.jp>
2021-07-29 22:54:47 +0900

    Added Miro.swift to the project

--- stts/Services/StatusPage/Miro.swift
+++ stts/Services/StatusPage/Miro.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 class Miro: StatusPageService {
-    let url = URL(string: "https://status.miro.com/")!
+    let url = URL(string: "https://status.miro.com")!
     let statusPageID = "trq7m2ycw1t2"
 }
