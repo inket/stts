be9a626703dca1c77a33acf5615d2bdf1f54d034
Mahdi Bchatnia <git@mahdi.jp>
2021-08-13 09:15:37 +0900

    Added ReadMe.swift to project using extract script

--- stts/Services/StatusPage/ReadMe.swift
+++ stts/Services/StatusPage/ReadMe.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 class ReadMe: StatusPageService {
-    let url = URL(string: "https://www.readmestatus.com/")!
+    let url = URL(string: "https://www.readmestatus.com")!
     let statusPageID = "nz3r09cr7dk1"
 }
