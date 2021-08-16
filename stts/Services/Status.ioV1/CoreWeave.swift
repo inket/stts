322a267bd5ebdf9d687b243537e3d8841c0ba927
Brandon Bianchi <brandon@brandonbianchi.com>
2021-08-16 15:02:00 -0700

    Create CoreWeave.swift
    Adding status page for CoreWeave to the status.io section.

---
+++ stts/Services/Status.ioV1/CoreWeave.swift
@@ -0,0 +1,11 @@
+//
+//  CoreWeave.swift
+//  stts
+//
+
+import Foundation
+
+class CoreWeave: StatusioV1Service {
+    let url = URL(string: "https://status.coreweave.com")!
+    let statusPageID = "5e126e998f2f032e1f8f0f4b"
+}
