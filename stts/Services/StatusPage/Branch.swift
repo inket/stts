afbe509852b8e53cbd59be1168eb8fb340a1b57a
Dick Tang <dick.tang@ymail.com>
2021-08-17 10:21:35 +0800

    add OneSignal, HashiCorp, Branch StatusPage

---
+++ stts/Services/StatusPage/Branch.swift
@@ -0,0 +1,11 @@
+//
+//  Branch.swift
+//  stts
+//
+
+import Foundation
+
+class Branch: StatusPageService {
+    let url = URL(string: "https://status.branch.io")!
+    let statusPageID = "74brn6fz1jt2"
+}
---
+++ stts/Services/StatusPage/OneSignal.swift
@@ -0,0 +1,11 @@
+//
+//  OneSignal.swift
+//  stts
+//
+
+import Foundation
+
+class OneSignal: StatusPageService {
+    let url = URL(string: "https://status.onesignal.com")!
+    let statusPageID = "wd0vc05tb8gm"
+}
