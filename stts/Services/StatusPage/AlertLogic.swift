34d7e81ad7c1b0958db7f37d605c1ae1cebcfd25
Mahdi Bchatnia <git@mahdi.jp>
2021-09-04 15:35:31 +0900

    Added files to project, fixed AlertLogic name

--- stts/Services/StatusPage/AlertLogic.swift
+++ stts/Services/StatusPage/AlertLogic.swift
@@ -6,6 +6,7 @@
 import Foundation
 
 class AlertLogic: StatusPageService {
+    let name = "Alert Logic"
     let url = URL(string: "https://status.alertlogic.com")!
     let statusPageID = "bkbjcmdhhfbp"
 }
