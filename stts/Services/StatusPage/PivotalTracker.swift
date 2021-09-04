a165dd0044e4416ac709eddba4ed91389fd81705
Mahdi Bchatnia <git@mahdi.jp>
2021-09-04 15:37:03 +0900

    Add a note about Pivotal Tracker being unavailable

--- stts/Services/StatusPage/PivotalTracker.swift
+++ stts/Services/StatusPage/PivotalTracker.swift
@@ -7,6 +7,6 @@ import Foundation
 
 class PivotalTracker: StatusPageService {
     let name = "Pivotal Tracker"
-    let url = URL(string: "https://status.pivotaltracker.com")!
+    let url = URL(string: "https://status.pivotaltracker.com")! // Unavailable as of 2021/9/4
     let statusPageID = "mjwp4vwtvdp8"
 }
