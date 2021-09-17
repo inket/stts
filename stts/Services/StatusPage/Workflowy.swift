0810a381477870aa46de3e2d3f46113e6d318d9c
Mahdi Bchatnia <git@mahdi.jp>
2021-09-17 21:09:11 +0900

    Added service: Workflowy, closes #151

---
+++ stts/Services/StatusPage/Workflowy.swift
@@ -0,0 +1,11 @@
+//
+//  Workflowy.swift
+//  stts
+//
+
+import Foundation
+
+class Workflowy: StatusPageService {
+    let url = URL(string: "https://status.workflowy.com")!
+    let statusPageID = "m8f43n57h83g"
+}
