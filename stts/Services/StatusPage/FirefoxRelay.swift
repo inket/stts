81b133e8353c69dff36af709eec8fe09fa410536
Ro Bin-Wan Känobi <32737410+tau-ri@users.noreply.github.com>
2021-11-07 15:23:23 +0100

    Create FirefoxRelay.swift
    add new Firefox Relay service

---
+++ stts/Services/StatusPage/FirefoxRelay.swift
@@ -0,0 +1,12 @@
+//
+//  FirefoxRelay.swift
+//  stts
+//
+
+import Foundation
+
+class FirefoxRelay: StatusPageService {
+    let name = "Firefox Relay"
+    let url = URL(string: "https://status.relay.firefox.com/")!
+    let statusPageID = "cpf95vpfj2db"
+}
