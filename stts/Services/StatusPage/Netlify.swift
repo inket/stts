8f50f4ffb86e68c63ae13674bf4af477deb18a2d
Mahdi Bchatnia <git@mahdi.jp>
2022-01-04 00:49:34 +0900

    Updated service: Netlify
    Use common implementation with other services since it also uses StatusPage

---
+++ stts/Services/StatusPage/Netlify.swift
@@ -0,0 +1,11 @@
+//
+//  Netlify.swift
+//  stts
+//
+
+import Foundation
+
+class Netlify: StatusPageService {
+    let url = URL(string: "https://www.netlifystatus.com")!
+    let statusPageID = "83dz7syz0j3x"
+}
