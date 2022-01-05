670fdbac34f37b74fff8f3e5ac9061a33a70602e
Josh Powell <josh@innovativeops.co>
2022-01-05 15:33:05 -0500

    Adds ClickUp status

---
+++ stts/Services/Status.ioV1/ClickUp.swift
@@ -0,0 +1,11 @@
+//
+//  ClickUp.swift
+//  stts
+//
+
+import Foundation
+
+class ClickUp: StatusioV1Service {
+    let url = URL(string: "https://status.clickup.com")!
+    let statusPageID = "5b6e0963c662144d00913a09"
+}
