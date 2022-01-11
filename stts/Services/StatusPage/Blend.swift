8645ef8ef12ecf9ff1a402a4241013a94030120a
Prashant Katwa <pkatwa@knock.com>
2022-01-11 16:02:33 -0500

    Add services Blend, Bugsnag Customerio, Dialpad, Kustomer

---
+++ stts/Services/StatusPage/Blend.swift
@@ -0,0 +1,11 @@
+//
+//  Blend.swift
+//  stts
+//
+
+import Foundation
+
+class Blend: StatusPageService {
+    let url = URL(string: "https://status.blend.com/")!
+    let statusPageID = "wp6n3nr9gm7w"
+}
---
+++ stts/Services/StatusPage/Customerio.swift
@@ -0,0 +1,12 @@
+//
+//  Customerio.swift
+//  stts
+//
+
+import Foundation
+
+class Customerio: StatusPageService {
+    let name = "Customer.io"
+    let url = URL(string: "https://status.customerio.com/")!
+    let statusPageID = "w4qr97njbszt"
+}
