cf7c8f662cda7f7c343a660fc6df496c62950c57
Mahdi Bchatnia <git@mahdi.jp>
2022-03-09 15:05:56 +0900

    Added service: PlayFab, closes #169

---
+++ stts/Services/StatusPage/PlayFab.swift
@@ -0,0 +1,11 @@
+//
+//  PlayFab.swift
+//  stts
+//
+
+import Foundation
+
+class PlayFab: StatusPageService {
+    let url = URL(string: "https://status.playfab.com")!
+    let statusPageID = "hndmlgmdcpdk"
+}
