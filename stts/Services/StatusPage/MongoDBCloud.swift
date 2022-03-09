6497921907aaa1442d966491a57828a96b0f92a5
Mahdi Bchatnia <git@mahdi.jp>
2022-03-09 15:10:16 +0900

    Added service: MongoDB Cloud

---
+++ stts/Services/StatusPage/MongoDBCloud.swift
@@ -0,0 +1,12 @@
+//
+//  MongoDBCloud.swift
+//  stts
+//
+
+import Foundation
+
+class MongoDBCloud: StatusPageService {
+    let name = "MongoDB Cloud"
+    let url = URL(string: "https://status.cloud.mongodb.com")!
+    let statusPageID = "smhnl3lt74kc"
+}
