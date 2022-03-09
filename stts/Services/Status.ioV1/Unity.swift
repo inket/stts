0daded32d628e273ee0a77c689f92fa198776043
Mahdi Bchatnia <git@mahdi.jp>
2022-03-09 15:01:34 +0900

    Added service: Unity, closes #166

---
+++ stts/Services/Status.ioV1/Unity.swift
@@ -0,0 +1,11 @@
+//
+//  Unity.swift
+//  stts
+//
+
+import Foundation
+
+class Unity: StatusioV1Service {
+    let url = URL(string: "https://status.cloud.unity3d.com")!
+    let statusPageID = "5519b5b522d275e510000088"
+}
