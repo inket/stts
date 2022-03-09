f551c83ab8e2976712bd95c061c3c45c77c80829
Mahdi Bchatnia <git@mahdi.jp>
2022-03-09 15:06:57 +0900

    Added service: MessageBird, closes #170

---
+++ stts/Services/StatusPage/MessageBird.swift
@@ -0,0 +1,11 @@
+//
+//  MessageBird.swift
+//  stts
+//
+
+import Foundation
+
+class MessageBird: StatusPageService {
+    let url = URL(string: "https://status.messagebird.com")!
+    let statusPageID = "xf8120tyqx8n"
+}
