45b683dea717ced1b7756979fbbe5bf6d07724a4
Mahdi Bchatnia <git@mahdi.jp>
2022-04-05 10:08:32 +0900

    Fixed/simplified DigitalOcean implementation
    Parsing page is unreliable

---
+++ stts/Services/StatusPage/DigitalOcean.swift
@@ -0,0 +1,11 @@
+//
+//  DigitalOcean.swift
+//  stts
+//
+
+import Foundation
+
+class DigitalOcean: StatusPageService {
+    let statusPageID = "s2k7tnzlhrpw"
+    let url = URL(string: "https://status.digitalocean.com")!
+}
