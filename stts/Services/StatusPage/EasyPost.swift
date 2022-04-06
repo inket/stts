2ee2d28b07c930673ba540c4706153353a21b2b1
Mahdi Bchatnia <git@mahdi.jp>
2022-04-06 09:56:45 +0900

    Added service EasyPost, closes #175

---
+++ stts/Services/StatusPage/EasyPost.swift
@@ -0,0 +1,11 @@
+//
+//  EasyPost.swift
+//  stts
+//
+
+import Foundation
+
+class EasyPost: StatusPageService {
+    let url = URL(string: "https://www.easypoststatus.com")!
+    let statusPageID = "n1jtz5983249"
+}
