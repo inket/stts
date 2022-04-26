5bd1ccc41ffb5dd8429ad1e343c78d1f2175e6f0
Dawnflash <adamzahumensky@gmail.com>
2022-04-26 20:47:43 +0200

    Add support for teleport.sh

---
+++ stts/Services/StatusPage/TeleportCloud.swift
@@ -0,0 +1,12 @@
+//
+//  TeleportCloud.swift
+//  stts
+//
+
+import Foundation
+
+class TeleportCloud: StatusPageService {
+    let name = "Teleport Cloud"
+    let url = URL(string: "https://status.teleport.sh")!
+    let statusPageID = "j16wc0h3wc86"
+}
