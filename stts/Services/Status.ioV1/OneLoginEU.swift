6f68cfb3ed7f1796f8a377eb18ac5557ff922883
Mahdi Bchatnia <git@mahdi.jp>
2022-04-30 08:23:21 +0900

    Added service: OneLogin, closes #176

---
+++ stts/Services/Status.ioV1/OneLoginEU.swift
@@ -0,0 +1,12 @@
+//
+//  OneLoginEU.swift
+//  stts
+//
+
+import Foundation
+
+class OneLoginEU: StatusioV1Service {
+    let name = "OneLogin (EU)"
+    let url = URL(string: "https://status.eu.onelogin.com")!
+    let statusPageID = "54da2ea0e6bd61bf0d00002b"
+}
---
+++ stts/Services/Status.ioV1/OneLoginUS.swift
@@ -0,0 +1,12 @@
+//
+//  OneLoginUS.swift
+//  stts
+//
+
+import Foundation
+
+class OneLoginUS: StatusioV1Service {
+    let name = "OneLogin (US)"
+    let url = URL(string: "https://status.us.onelogin.com")!
+    let statusPageID = "538511e2ce5cb97537000144"
+}
