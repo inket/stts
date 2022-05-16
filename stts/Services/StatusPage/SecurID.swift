4c9e23e27dfd83b1f71c27071b5ddc7d20f03ea0
Geoffrey Hichborn <ghichborn@vmware.com>
2022-05-16 09:59:16 -0700

    Add RSA SecurID

---
+++ stts/Services/StatusPage/SecurID.swift
@@ -0,0 +1,11 @@
+//
+//  SecurID.swift
+//  stts
+//
+
+import Foundation
+
+class SecurID: StatusPageService {
+    let url = URL(string: "https://status.securid.com")!
+    let statusPageID = "1xw7gmwkm0bg"
+}
