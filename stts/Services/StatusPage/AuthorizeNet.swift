0e9f16ea9aa85786c3a117d557e1388681b3e558
Mahdi Bchatnia <git@mahdi.jp>
2022-07-10 03:03:26 +0900

    Update Authorize.Net to use StatusPage
    Authorize.Net page was changed recently

---
+++ stts/Services/StatusPage/AuthorizeNet.swift
@@ -0,0 +1,12 @@
+//
+//  AuthorizeNet.swift
+//  stts
+//
+
+import Foundation
+
+class AuthorizeNet: StatusPageService {
+    let name = "Authorize.Net"
+    let url = URL(string: "https://status.authorize.net")!
+    let statusPageID = "06v575cbzlpr"
+}
