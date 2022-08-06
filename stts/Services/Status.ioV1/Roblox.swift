3e5cede7b9294e7da4dc156633a5aee8ee27553c
Mahdi Bchatnia <git@mahdi.jp>
2022-08-06 10:56:13 +0900

    Added service: Roblox, closes #184

---
+++ stts/Services/Status.ioV1/Roblox.swift
@@ -0,0 +1,11 @@
+//
+//  Roblox.swift
+//  stts
+//
+
+import Foundation
+
+class Roblox: StatusioV1Service {
+    let url = URL(string: "https://status.roblox.com")!
+    let statusPageID = "59db90dbcdeb2f04dadcf16d"
+}
