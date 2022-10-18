d37828d883766d366c942eeae2b94e90d706e04f
Mahdi Bchatnia <git@mahdi.jp>
2022-10-18 09:49:00 +0900

    Added service: Scaleway, closes #188

---
+++ stts/Services/StatusPage/Scaleway.swift
@@ -0,0 +1,11 @@
+//
+//  Scaleway.swift
+//  stts
+//
+
+import Foundation
+
+class Scaleway: StatusPageService {
+    let url = URL(string: "https://status.scaleway.com")!
+    let statusPageID = "s2kbtscly3pj"
+}
