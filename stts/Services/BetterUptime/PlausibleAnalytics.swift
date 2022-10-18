a0e5461b2cedb5b733c41821234dc04445b39925
Mahdi Bchatnia <git@mahdi.jp>
2022-10-18 14:52:09 +0900

    Fixed service: Plausible Analytics, closes #189

---
+++ stts/Services/BetterUptime/PlausibleAnalytics.swift
@@ -0,0 +1,11 @@
+//
+//  PlausibleAnalytics.swift
+//  stts
+//
+
+import Foundation
+
+class PlausibleAnalytics: BetterUptimeService {
+    let name = "Plausible Analytics"
+    let url = URL(string: "https://status.plausible.io")!
+}
