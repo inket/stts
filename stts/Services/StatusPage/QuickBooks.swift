7960a668c976313d82e29692b5c7cac1cb70b065
Mahdi Bchatnia <git@mahdi.jp>
2022-10-18 09:47:58 +0900

    Added service: QuickBooks, closes #187

---
+++ stts/Services/StatusPage/QuickBooks.swift
@@ -0,0 +1,11 @@
+//
+//  QuickBooks.swift
+//  stts
+//
+
+import Foundation
+
+class QuickBooks: StatusPageService {
+    let url = URL(string: "https://status.quickbooks.intuit.com")!
+    let statusPageID = "2dv2gqw9hrgc"
+}
