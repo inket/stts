f196068e58cc12f268458a8421176531645d7c6b
Mahdi Bchatnia <git@mahdi.jp>
2022-10-28 21:37:09 +0900

    Added service: Keeper Security, closes #190

---
+++ stts/Services/StatusPage/KeeperSecurity.swift
@@ -0,0 +1,12 @@
+//
+//  KeeperSecurity.swift
+//  stts
+//
+
+import Foundation
+
+class KeeperSecurity: StatusPageService {
+    let name = "Keeper Security"
+    let url = URL(string: "https://statuspage.keeper.io")!
+    let statusPageID = "wg5bvczw1rtf"
+}
