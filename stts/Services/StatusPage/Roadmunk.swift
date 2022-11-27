d1d6e7ea2f6954db7f1aa215493ff694b2484fae
Mahdi Bchatnia <git@mahdi.jp>
2022-11-27 16:37:01 +0900

    Added service: Roadmunk, closes #195

---
+++ stts/Services/StatusPage/Roadmunk.swift
@@ -0,0 +1,11 @@
+//
+//  Roadmunk.swift
+//  stts
+//
+
+import Foundation
+
+class Roadmunk: StatusPageService {
+    let url = URL(string: "https://status.roadmunk.com/")!
+    let statusPageID = "wfwzfc29bq4r"
+}
