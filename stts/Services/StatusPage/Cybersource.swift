49d1f961b68c4bd6f14552bff4ccba5909716cf5
Mahdi Bchatnia <git@mahdi.jp>
2022-11-27 16:36:31 +0900

    Added service: Cybersource, closes #194

---
+++ stts/Services/StatusPage/Cybersource.swift
@@ -0,0 +1,11 @@
+//
+//  Cybersource.swift
+//  stts
+//
+
+import Foundation
+
+class Cybersource: StatusPageService {
+    let url = URL(string: "https://status.cybersource.com")!
+    let statusPageID = "ttbtd8wrjg1b"
+}
