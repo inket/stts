fdf661a39bf8063ac2a9dacbe8beca38dae6d673
Mahdi Bchatnia <git@mahdi.jp>
2023-03-14 12:41:41 +0100

    Added service: mastodon.social

---
+++ stts/Services/Instatus/MastodonSocial.swift
@@ -0,0 +1,11 @@
+//
+//  MastodonSocial.swift
+//  stts
+//
+
+import Foundation
+
+class MastodonSocial: InstatusService {
+    let name = "mastodon.social"
+    let url = URL(string: "https://status.mastodon.social")!
+}
