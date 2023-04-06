3ec5a6599e68cdce8387174d32955192514441c6
Gregory Daynes <email@gregdaynes.com>
2023-04-06 14:37:53 -0700

    add fly.io status page
    Add support for hosting provider Fly.io status page.

---
+++ stts/Services/StatusPage/Flyio.swift
@@ -0,0 +1,12 @@
+//
+//  Flyio.swift
+//  stts
+//
+
+import Foundation
+
+class Flyio: StatusPageService {
+    let name = "Fly.io"
+    let url = URL(string: "https://status.flyio.net/")!
+    let statusPageID = "65rccr4mbblw"
+}
