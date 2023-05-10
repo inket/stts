f4bed6151c2245e60906ecb510892a65ad23fb54
marcaube <marcaube@users.noreply.github.com>
2023-05-10 13:12:56 -0400

    Add Lattice status page

---
+++ stts/Services/StatusPage/Lattice.swift
@@ -0,0 +1,11 @@
+//
+//  Lattice.swift
+//  stts
+//
+
+import Foundation
+
+class Lattice: StatusPageService {
+    let url = URL(string: "https://status.lattice.com")!
+    let statusPageID = "gj13f8gmt9b6"
+}
