8a85741f8faa502ca412fbd1a281b33efe5e98f9
marcaube <marcaube@users.noreply.github.com>
2023-06-13 16:51:33 -0400

    Add Robin status page

---
+++ stts/Services/StatusPage/Robin.swift
@@ -0,0 +1,11 @@
+//
+//  Robin.swift
+//  stts
+//
+
+import Foundation
+
+class Robin: StatusPageService {
+    let url = URL(string: "https://status.robinpowered.com")!
+    let statusPageID = "4hy25vm4h6fc"
+}
