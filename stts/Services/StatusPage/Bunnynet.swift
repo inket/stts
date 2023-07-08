e6638880387aa4bef6572a8ca63a0a700d97b1df
Mahdi Bchatnia <git@mahdi.jp>
2023-07-08 23:54:06 +0900

    Added service bunny.net, closes #215

---
+++ stts/Services/StatusPage/Bunnynet.swift
@@ -0,0 +1,12 @@
+//
+//  Bunnynet.swift
+//  stts
+//
+
+import Foundation
+
+class Bunnynet: StatusPageService {
+    let name = "bunny.net"
+    let url = URL(string: "https://status.bunny.net")!
+    let statusPageID = "jggkjgplmcdc"
+}
