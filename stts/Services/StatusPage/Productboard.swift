66139e0fa759cd9f0a88fdbfb47d1315ecac1610
tukak <lukas.kutner@productboard.com>
2023-07-25 14:21:17 +0200

    Add Productboard status page

---
+++ stts/Services/StatusPage/Productboard.swift
@@ -0,0 +1,11 @@
+//
+//  Productboard.swift
+//  stts
+//
+
+import Foundation
+
+class Productboard: StatusPageService {
+    let url = URL(string: "https://status.productboard.com/")!
+    let statusPageID = "wwwnvh1nlpt1"
+}
