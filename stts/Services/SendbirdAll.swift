9efe03aece565b8a387a64f49843069d0b9d194d
Mahdi Bchatnia <git@mahdi.jp>
2023-08-11 10:01:06 +0900

    Senbird (All): display more detailed status message

--- stts/Services/SendbirdAll.swift
+++ stts/Services/SendbirdAll.swift
@@ -28,16 +28,26 @@ class SendbirdAll: IndependentService, ServiceCategory {
         group.notify(queue: .main) { [weak self] in
             guard let self else { return }
 
-            let worstStatus = sendbirdServices.map { $0.status }.max() ?? .undetermined
-            let message: String
+            var messageComponents: [String] = []
+            for service in Self.sendbirdServices {
+                if service.status != .good {
+                    messageComponents.append(service.name)
+                    messageComponents.append("* \(service.message)")
+                }
+            }
 
-            switch worstStatus {
-            case .undetermined: message = "Unexpected response"
-            case .good: message = "Operational"
-            case .minor: message = "Minor outage"
-            case .major: message = "Major outage"
-            case .notice: message = "Degraded service"
-            case .maintenance: message = "Maintenance"
+            let worstStatus = Self.sendbirdServices.map { $0.status }.max() ?? .undetermined
+            var message = messageComponents.joined(separator: "\n")
+
+            if message.isEmpty {
+                switch worstStatus {
+                case .undetermined: message = "Unexpected response"
+                case .good: message = "Operational"
+                case .minor: message = "Minor outage"
+                case .major: message = "Major outage"
+                case .notice: message = "Degraded service"
+                case .maintenance: message = "Maintenance"
+                }
             }
 
             statusDescription = ServiceStatusDescription(status: worstStatus, message: message)
