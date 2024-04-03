1d0bd42feeb07367d3b2b896d2d64cf5171562ce
Mahdi Bchatnia <git@mahdi.jp>
2024-04-03 21:17:07 +0900

    Replaced service QuayIO with Red Hat

---
+++ stts/Services/StatusPage/RedHat.swift
@@ -0,0 +1,12 @@
+//
+//  RedHat.swift
+//  stts
+//
+
+import Foundation
+
+class RedHat: StatusPageService {
+    let name = "Red Hat"
+    let url = URL(string: "https://status.redhat.com")!
+    let statusPageID = "dn6mqn7xvzz3"
+}
