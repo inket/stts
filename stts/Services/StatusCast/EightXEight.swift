e94cb0b3404a8ccb083be4e642684c6886e9eb53
Mahdi Bchatnia <git@mahdi.jp>
2024-07-13 08:23:50 +0900

    Added service: 8x8

---
+++ stts/Services/StatusCast/EightXEight.swift
@@ -0,0 +1,12 @@
+//
+//  EightXEight.swift
+//  stts
+//
+
+import Foundation
+
+class EightXEight: StatusCastService {
+    let name = "8x8"
+    let hasCurrentStatus = false
+    let url = URL(string: "https://status.8x8.com")!
+}
