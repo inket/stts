6fbfbd2c7a912b13bf4a2fd26728d5f58c431c02
Mahdi Bchatnia <git@mahdi.jp>
2024-07-13 08:23:06 +0900

    Added service: jsDelivr

---
+++ stts/Services/UptimeDotCom/JSDelivr.swift
@@ -0,0 +1,11 @@
+//
+//  JSDelivr.swift
+//  stts
+//
+
+import Foundation
+
+class JSDelivr: UptimeDotComService {
+    let name = "jsDelivr"
+    let url = URL(string: "https://status.jsdelivr.com")!
+}
