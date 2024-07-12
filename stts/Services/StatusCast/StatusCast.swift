e9eee3147f7034fa9d563613e9f8de6ae90882b3
Mahdi Bchatnia <git@mahdi.jp>
2024-07-13 08:23:43 +0900

    Added service: StatusCast

---
+++ stts/Services/StatusCast/StatusCast.swift
@@ -0,0 +1,11 @@
+//
+//  StatusCast.swift
+//  stts
+//
+
+import Foundation
+
+class StatusCast: StatusCastService {
+    let hasCurrentStatus = false
+    let url = URL(string: "https://status.statuscast.com")!
+}
