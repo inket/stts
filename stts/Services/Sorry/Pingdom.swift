dba4e8263e3223757332759a6f8113a564f0ab45
Mahdi Bchatnia <git@mahdi.jp>
2024-07-12 21:06:10 +0900

    Add comment about Pingdom page id

--- stts/Services/Sorry/Pingdom.swift
+++ stts/Services/Sorry/Pingdom.swift
@@ -7,5 +7,5 @@ import Foundation
 
 class Pingdom: SorryService {
     let url = URL(string: "https://status.pingdom.com")!
-    let pageID = "9c53640d"
+    let pageID = "9c53640d" // Page ID was found on page back in 2018, and is now nowhere to be found
 }
