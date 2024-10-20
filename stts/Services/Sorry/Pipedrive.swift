41e4d30b1ad54b0197c14017dd7f6fce765f3949
Mahdi Bchatnia <git@mahdi.jp>
2024-10-21 00:17:34 +0900

    Add services: Pipedrive and Postmark, closes #223

---
+++ stts/Services/Sorry/Pipedrive.swift
@@ -0,0 +1,11 @@
+//
+//  Pipedrive.swift
+//  stts
+//
+
+import Foundation
+
+class Pipedrive: SorryService {
+    let url = URL(string: "https://status.pipedrive.com")!
+    let pageID = "2754"
+}
---
+++ stts/Services/Sorry/Postmark.swift
@@ -0,0 +1,11 @@
+//
+//  Postmark.swift
+//  stts
+//
+
+import Foundation
+
+class Postmark: SorryService {
+    let url = URL(string: "https://status.postmarkapp.com")!
+    let pageID = "12903"
+}
