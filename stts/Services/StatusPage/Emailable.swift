20593680ce5f475679256c77be9b5b0223e798e1
Mahdi Bchatnia <git@mahdi.jp>
2024-10-21 00:21:31 +0900

    Added service: Emailable, closes #224

---
+++ stts/Services/StatusPage/Emailable.swift
@@ -0,0 +1,11 @@
+//
+//  Emailable.swift
+//  stts
+//
+
+import Foundation
+
+class Emailable: StatusPageService {
+    let url = URL(string: "https://status.emailable.com")!
+    let statusPageID = "ntzxwsjy15bq"
+}
