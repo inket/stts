fb95b3fe72c8bcc248df5116e82a52f1ccc72f99
MatiasFernandez <mfernandez@10pines.com>
2025-11-25 16:56:42 -0300

    Add Cursor status page

---
+++ stts/Services/StatusPage/Cursor.swift
@@ -0,0 +1,11 @@
+//
+//  Cursor.swift
+//  stts
+//
+
+import Foundation
+
+class Cursor: StatusPageService {
+    let url = URL(string: "https://status.cursor.com/")!
+    let statusPageID = "0tp9ssgtptvs"
+}
